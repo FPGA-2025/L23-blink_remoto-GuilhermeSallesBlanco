c28cdcf99b425fc90ae6e21e728d21f17daa7af0
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2025-06-29 23:54:30 -0300

    Quarto teste (final)

--- blink.v
+++ blink.v
@@ -6,27 +6,42 @@ module Blink #(
     output reg [7:0] leds
 );
 
-localparam ONE_SECOND = CLK_FREQ;
-localparam HALF_SECOND = CLK_FREQ / 2;
-localparam QUARTER_SECOND = CLK_FREQ / 4;
+localparam ON_TIME = (CLK_FREQ * 3) / 4;   // 75% ON
+localparam OFF_TIME = CLK_FREQ / 4;        // 25% OFF
 
 reg [31:0] counter;
+reg state; // 0: ON, 1: OFF
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
         counter <= 32'b0;
         leds <= 8'b0;
+        state <= 0;
     end else begin
-        if(counter >= QUARTER_SECOND - 1) begin
-            counter <= 32'b0;
-            leds [0] <= ~leds[0];
-            leds [2] <= ~leds[2];
-            leds [4] <= ~leds[4];
-            leds [6] <= ~leds[6];
-        end else begin
-            counter <= counter + 1;
+        if(state == 0) begin 
+            leds[0] <= 1'b1;
+            leds[2] <= 1'b1;
+            leds[4] <= 1'b1;
+            leds[6] <= 1'b1;
+            if(counter >= ON_TIME - 1) begin
+                counter <= 32'b0;
+                state <= 1;
+            end else begin
+                counter <= counter + 1;
+            end
+        end else begin 
+            leds[0] <= 1'b0;
+            leds[2] <= 1'b0;
+            leds[4] <= 1'b0;
+            leds[6] <= 1'b0;
+            if(counter >= OFF_TIME - 1) begin
+                counter <= 32'b0;
+                state <= 0;
+            end else begin
+                counter <= counter + 1;
+            end
         end
     end
 end
-    
+
 endmodule
